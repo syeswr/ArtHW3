e736986f22a9730ed54bd6a7ad6f9e47823d60eb
Yihao Zhu <yihao.zhu@icloud.com>
2017-11-11 12:39:21 -0500

    First submission

---
+++ main.cpp
@@ -0,0 +1,43 @@
+#include <string>
+#include <iostream>
+using namespace std;
+void of_time_void_of_life(string &of_flowers_falling, int o_abyss, int o_past){
+    /*In front of my footpr*/int I_stopped = /*looking in t*/ o_abyss;
+    /*In front of my last footpr*/int I_look_back = /*looking in t*/ o_past;
+    while(I_stopped<I_look_back){
+        char ed=of_flowers_falling[I_stopped]; /*crying*/
+        /*Moments, */of_flowers_falling[I_stopped++]=of_flowers_falling[I_look_back]; /*looking into my died dreams*/
+        /*Are there any*/of_flowers_falling[I_look_back--]=ed; /*it my memory, ask my self*/
+    }
+}
+
+void swallows_everything_I_can_never_escape_that(string &of_grievous_death) {
+
+    /*Never H*/int me=0, my_past=0;
+    /*Never H*/int me_my_soul=0;
+    /*Never H*/int me_my_destiny=of_grievous_death.length();/*of my life, is now to the end*/
+    /*Never H*/int me_please=0;
+    /*Po*/int at = me_my_destiny;
+    while(true){ /*hero lasts*/
+        while(me<at && of_grievous_death[me] == ' '){/*aningfulness, while*/ me++;}/*at is my body*/
+        if(me==at) break; /*the law of soul*/
+        if(me_please){/*get me out of there, get me out*/of_grievous_death[my_past++]=' ';} /*why always counts?*/
+        /*Forgive*/ me_my_soul=my_past; /*even my none-existing future*/
+        while(me<at && of_grievous_death[me] != ' ') {/*et soul of glorious sacrifice*/
+            /*My future*/of_grievous_death[my_past]=of_grievous_death[me]; /*et, It's killing*/ my_past++; /*It's vanishing*/ me++;}
+        /*Void*/ of_time_void_of_life(of_grievous_death,me_my_soul,my_past-1);/*my body, burns into dust, in vain*/
+        /*Never help*/me_please++;
+
+    }
+
+    /*None of those strings*/of_grievous_death.resize(my_past);
+    /*Void*/of_time_void_of_life(of_grievous_death,0,my_past-1); /*now is my whole life*/
+}
+
+/*Sample usage*/
+int main(){
+    string makes_the_story = "I do not derserve";
+    swallows_everything_I_can_never_escape_that(makes_the_story);
+    cout<<makes_the_story;
+    //The output will be "derserve not do I"
+}
